eb191a7002f301bac3a761b203ccd5100f8c9943
Felipe Hummel <felipehummel@gmail.com>
2011-11-26 16:51:17 -0400

    First example usage of MemoryIndex on main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <QDebug>
+#include <QStringList>
 
+#include "index/memoryindex.h"
 
 int main(int argc, char *argv[])
 {
+    Q_UNUSED(argc); Q_UNUSED(argv)
+    Index *index = new MemoryIndex();
+    QStringList list;
+    list << "teste";
+    list << "teste1";
+    list << "teste1";
+    list << "teste1";
+    list << "teste1";
+    list << "teste2";
+    index->addDocument(list);
+    PostingListIterator *it = index->getPostingList(QString("teste1"));
+    qDebug() << "AKI: " << it->hasNext();
+    while (it->hasNext()) {
+        Posting posting = it->next();
+        qDebug() << posting.toString();
+    }
     return 1;
 }
