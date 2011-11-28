0dc2fb4f1d0e8a990f4cecc223a448b6f6466cc2
Felipe Hummel <felipehummel@gmail.com>
2011-11-28 10:29:25 -0400

    Added indexing of file through cli arguments

--- main.cpp
+++ main.cpp
@@ -7,16 +7,16 @@
 
 int main(int argc, char *argv[])
 {
-    Q_UNUSED(argc); Q_UNUSED(argv)
     Index *index = new MemoryIndex();
     Indexer indexer(index, Tokenizer());
-    indexer.indexDocument("a casa do cachorro foi parar lá distante longe pra caramba");
-    indexer.indexDocument("teste blablabla cara caramba caraô casa do caramba");
-    indexer.indexDocument("eu quis dizer você não quis escutar não quero promessas");
-    indexer.indexDocument("quero te dizer que você vai se lascar");
+    indexer.indexDocument("example document");
+    indexer.indexDocument("example document two");
+    if (argc > 1) {
+        indexer.indexFileDocPerLine(QString(argv[1]));
+    }
 
     QueryProcessor processor(index, Tokenizer());
-    QList<Result> results = processor.searchAND("casa");
+    QList<Result> results = processor.searchAND("example");
     foreach (Result result, results) {
         qDebug () << result.score << " :: " << index->doc(result.docId);
     }
