5d5d3460bfd699fe0b6d38cddbcadcaf3bd1ac7b
Maisa Vidal <ext-maisa.teixeira@nokia.com>
2012-01-09 10:09:42 -0400

    Add licence LGPL to each source file and also AUTHORS file
    Signed-off-by: Maisa Vidal <ext-maisa.teixeira@nokia.com>

--- analyzer/tokenstream.h
+++ analyzer/tokenstream.h
@@ -1,3 +1,25 @@
+/*******************************************************************************
+**
+** This file is part of QtSearch **
+**
+**
+** Name: Qt Search
+** Version: Beta
+** Contact: Felipe da Costa Hummel <felipehummel@gmail.com>
+**          Maisa Vidal Teixeira <maisa.vidal@openbossa.org>
+**
+** Qt Search is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** any later version.
+**
+** Qt Search is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser General Public License for more details.
+**
+*******************************************************************************/
+
 #ifndef TOKENSTREAM_H
 #define TOKENSTREAM_H
 
